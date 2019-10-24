57939003fb2f33671b4a379b8ece6783da113018
Idan Cohen <idan@idancohen.com>
2019-10-24 22:34:08 +0300

    add functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,3 +6,18 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+export const addExclemation = functions.firestore
+  .document('messages/{messageId}')
+  .onCreate((snapshot, context) => {
+    // Get an object representing the document
+    const newValue = snapshot.data() || {};
+
+    // access a particular field as you would any JS property
+    const text = newValue.text + '!';
+
+    // return a promise to set the newly created document
+    return snapshot.ref.set({
+      text
+    }, {merge: true});
+  });
